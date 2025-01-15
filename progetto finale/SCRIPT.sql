39905907f7792d727f5c96ab1004c361838c49b9
alessio176 <alessio176@gmail.com>
2025-01-15 20:09:56 +0100

    final comments and clean up in final project

--- progetto finale/SCRIPT.sql
+++ progetto finale/SCRIPT.sql
@@ -26,99 +26,54 @@ Indicatori sulle transazioni per tipologia di conto
 
 */
 
-use banca;
-
--- First of all let's inspect tables, wich columns they have and fields' type (use the queries below or a db-client UI):
-
-/*
-show columns from cliente;
-
-show columns from conto;
-
-show columns from tipo_conto;
-
-show columns from tipo_transazione;
-
-show columns from transazioni;
-*/
-
-
--- Temporary queries:
--- customer age
-/*
- *  DATEDIFF da differenza in GIORNI.
- *  Da giorni ad anno: 1 y = 365 d -> 1 d = 1/365 y e poi si arrotonda verso lo 0 (no anni bisestili)
+/**
+ * Lo script è suddiviso in due parti:
+ * 1. nella prima parte sono definite delle viste (view) che servono da supporto per il caricamento dei record nella tabella finale
+ * 	orientativamente vi è una vista per indicatore.
+ * 2. nella seconda parte si crea la tabella finale (tab_finale) che contiene le fetaures richieste.
+ * 
+ * In ogni caso si rimanda ai commenti delle varie sezioni per ulteriori dettagli.
+ * 
+ * Nonostante le viste siano preservate nel database, anche dopo la disconnessione, questo script può essere lanciato diverse volte,
+ * producendo sempre lo stesso risultato in quanto ad ogni run le viste precedentemente ottenute verranno eliminate (tramite drop) e create nuovamente.
+ * Si adottano delle viste piuttosto che delle tabelle temporanee, in modo che i risultati parziali siano conservati anche in riconnessioni successive,
+ * per eventuali analisi parziali aggiuntive (le tabelle temporanee verrebbero eliminate alla disconnessione dal database).
  */
 
--- TODO: convertire tutte le view in temporary table (in modo da non salvarle definitivamente nel db).
--- fai questo solo come ultimo step, quando hai finito tutto (le view sono comode per ispezione tramite dbeaver).
+use banca;
 
--- TODO: rivedere le join (ad es. su id_cliente deve esserci una right join con cliente)
+/**
+ * 
+ * 
+ * Parte 1 - creazione delle viste di supporto
+ * 
+ * 
+ */
+
+-- età del cliente
+
+/*
+ *  Per il calcolo dell'età si usa DATEDIFF che dà la differenza tra data corrente e data di nascita in giorni.
+ *  Da giorni ad anno: 1 y = 365 d -> 1 d = 1/365 y e poi si arrotonda verso lo 0 (non si considerano anni bisestili).
+ * 
+ * 	Per comodità nella vista si includono anche gli altri campi dei clienti (utile per ispezione tramite client).
+ */
 
 drop view if exists cliente_tmp;
 create view cliente_tmp as 
 select *,FLOOR(DATEDIFF(CURRENT_DATE(),data_nascita)/365) as eta from cliente;
 
+-- check
 select * from cliente_tmp limit 10;
 
 -- Numero di transazioni in uscita su tutti i conti.
-/**
- * qui non importa il tipo di conto, quindi le tabelle interessate sono:
- * transazioni e tipo_transazione. 
- * transazione in uscita = spesa, quindi dove tipo_transazione ha segno -
- */
-
-drop view if exists num_trans_out;
-/*
-create view num_trans_out as (
-	select 
-	clien.id_cliente as id_cliente,
-	COUNT(*) as value
-	from transazioni t
-	inner join tipo_transazione tipo_t on t.id_tipo_trans = tipo_t.id_tipo_transazione 
-	inner join conto c on t.id_conto = c.id_conto 
-	inner join cliente clien on c.id_cliente = clien.id_cliente 
-	where tipo_t.segno = '-'
-	group by clien.id_cliente
-	order by id_cliente asc
-);
-select * from num_trans_out limit 10;
-*/
-
 -- Numero di transazioni in entrata su tutti i conti.
+
 /**
- * lo stesso di sopra ma dove segno = '+'
+ * Si costruisce unica vista dove le transazioni in uscita sono identificate dal campo tipo = 'spesa' 
+ * e quelle in entrata con tipo = 'accredito'.
  */
 
-drop view if exists num_trans_in;
-/*
-create view num_trans_in as (
-	select
-	clien.id_cliente as id_cliente,
-	COUNT(*) as value
-	from transazioni t
-	inner join tipo_transazione tipo_t on t.id_tipo_trans = tipo_t.id_tipo_transazione 
-	inner join conto c on t.id_conto = c.id_conto
-	inner join cliente clien on c.id_cliente = clien.id_cliente 
-	where tipo_t.segno = '+'
-	group by clien.id_cliente
-	order by id_cliente asc
-);
-select * from num_trans_in limit 10;
-*/
-
--- check: if everything is fine then, in below query, same_total = 1
-/*
-select (
-	(select value from num_trans_in) + (select value from num_trans_out)
-) = (select count(*) from transazioni) as same_total;
-
-select 12340 + 2345; -- 14685
-select count(*) from transazioni t; -- 14685
-*/
-
-
--- 	OPPURE UNICA VIEW
 drop view if exists num_trans;
 create view num_trans as (
 	select
@@ -135,58 +90,19 @@ create view num_trans as (
 	group by clien.id_cliente, tipo_t.segno
 	order by id_cliente asc
 );
+
+-- check
 select * from num_trans limit 10;
 
 
 
 -- Importo totale transato in uscita su tutti i conti.
-/**
- * Qui bisogna fare sum dell'importo (tab. transazione) invece del count.
- *  
- */
-
-drop view if exists tot_trans_out;
-/*
-create view tot_trans_out as (
-	select
-	clien.id_cliente as id_cliente,
-	sum(importo) as totale
-	from transazioni t
-	inner join tipo_transazione tipo_t on t.id_tipo_trans = tipo_t.id_tipo_transazione 
-	inner join conto c on t.id_conto = c.id_conto 
-	inner join cliente clien on c.id_cliente = clien.id_cliente 
-	where tipo_t.segno = '-'
-	group by clien.id_cliente
-	order by id_cliente asc
-);
-select * from tot_trans_out limit 10;
-*/
-
 -- Importo totale transato in entrata su tutti i conti.
+
 /**
- * Qui bisogna fare sum dell'importo (tab. transazione) invece del count.
- *  
+ * Anche qui si costruisce un'unica vista con la stessa logica della vista precedente 
+ * (in uscita -> tipo = 'spesa', in entrata -> tipo = 'accredito').
  */
-
-drop view if exists tot_trans_in;
-/*
-create view tot_trans_in as (
-	select
-	clien.id_cliente as id_cliente,
-	sum(importo) as totale
-	from transazioni t
-	inner join tipo_transazione tipo_t on t.id_tipo_trans = tipo_t.id_tipo_transazione 
-	inner join conto c on t.id_conto = c.id_conto 
-	inner join cliente clien on c.id_cliente = clien.id_cliente 
-	where tipo_t.segno = '+'
-	group by clien.id_cliente
-	order by id_cliente asc
-);
-select * from tot_trans_in limit 10;
-
-*/
-
--- 	OPPURE UNICA VIEW
 
 drop view if exists tot_trans;
 create view tot_trans as (
@@ -205,33 +121,10 @@ select
 	order by id_cliente asc
 );
 
+-- check
 select * from tot_trans limit 10;
 
--- come tabella temporanea 
-/*
-
-create temporary table tot_trans_table as (
-select
-	clien.id_cliente as id_cliente,
-	CASE
-		when tipo_t.segno = '+' then 'accredito'
-		else 'spesa'
-	end as tipo,
-	sum(importo) as totale
-	from transazioni t
-	inner join tipo_transazione tipo_t on t.id_tipo_trans = tipo_t.id_tipo_transazione 
-	inner join conto c on t.id_conto = c.id_conto 
-	inner join cliente clien on c.id_cliente = clien.id_cliente 
-	group by clien.id_cliente, tipo_t.segno
-	order by id_cliente asc
-);
-
-select * from tot_trans_table limit 10;
-
-*/
-
 -- Numero totale di conti posseduti.
--- qui non importa la tipologia di conto
 
 drop view if exists num_tot_conti;
 create view num_tot_conti as (
@@ -244,11 +137,15 @@ create view num_tot_conti as (
 	order by id_cliente asc
 );
 
+-- check
 select * from num_tot_conti limit 10;
 
 
 -- Numero di conti posseduti per tipologia (un indicatore per ogni tipo di conto).
--- lo stesso appena fatto, ma diviso per tipo di conto
+
+/**
+ * Questa vista è simile a quella appena precedente, ma si inserisce un ulteriore raggruppamento per tipo di conto.
+ */
 
 drop view if exists num_tot_conti_tipologia;
 create view num_tot_conti_tipologia as (
@@ -257,53 +154,23 @@ create view num_tot_conti_tipologia as (
 		tc.desc_tipo_conto as tipo,
 		COUNT(c.id_conto) as num_accounts
 	from conto c
-	inner join tipo_conto tc on c.id_tipo_conto = tc.id_tipo_conto -- prima inner join
+	inner join tipo_conto tc on c.id_tipo_conto = tc.id_tipo_conto
 	inner join cliente clien on c.id_cliente = clien.id_cliente
 	group by clien.id_cliente, tc.desc_tipo_conto
 	order by clien.id_cliente asc
 );
 
+-- check
 select * from num_tot_conti_tipologia limit 50;
-
--- nella view di qui sopra NON serve fare join con tipo_conto. Posso sfruttare la colonna id_tipo_conto di conto che è lo stesso della join alla fine.
-select 
-clien.id_cliente,
-c.id_tipo_conto,
-count(c.id_conto) as num_accounts
-from conto c
-right join cliente clien on c.id_cliente = clien.id_cliente
-group by clien.id_cliente,c.id_tipo_conto
-order by clien.id_cliente asc;
-
--- forse ci sono
-select 
-count(*)
-from conto c
-right join cliente clien on c.id_cliente = clien.id_cliente
-where clien.id_cliente = 2 and c.id_tipo_conto = 1;
-
-
-select 
-clien.id_cliente,
-count(*) as num
-from conto c
-right join cliente clien on c.id_cliente = clien.id_cliente
-where c.id_tipo_conto = 0 -- replicare pure per gli altri valori di id_conto
-group by clien.id_cliente
-order by clien.id_cliente asc;
-
-/*
- * nella tabella finale inizializzo i contatori a 0 (quando creo la tabella metto un default value a 0, oppure faccio poi un primo update)
- * POI
- * faccio un update prendendo i valori dalla select di qui sopra e così il contatore si aggiorna solo dove c'è un record.
- */
-
-
 
 -- Numero di transazioni in uscita per tipologia di conto (un indicatore per tipo di conto).
 -- Numero di transazioni in entrata per tipologia di conto (un indicatore per tipo di conto).
--- faccio questi due indicatori in unica view (come per il caso generale senza suddivisione per tipo conto)
 
+/**
+ * Basandosi sulla stessa logica delle altre viste, anche qui si fa un'unica vista per questi due contatori,
+ * sfruttando il raggruppamento dei record in base alla tipologia di conto e di transazione 
+ * (in uscita ->  tipo_transazione = 'spesa', in entrata -> tipo_transazione = 'accredito').
+ */
 
 drop view if exists num_trans_tipologia;
 create view num_trans_tipologia as (
@@ -323,12 +190,19 @@ create view num_trans_tipologia as (
 	group by clien.id_cliente, tipo_t.segno, tc.desc_tipo_conto
 	order by id_cliente asc
 );
+
+-- check
 select * from num_trans_tipologia limit 50;
 
 
 -- Importo transato in uscita per tipologia di conto (un indicatore per tipo di conto).
 -- Importo transato in entrata per tipologia di conto (un indicatore per tipo di conto).
--- faccio questi due indicatori in unica view (come per il caso generale senza suddivisione per tipo conto)
+
+/**
+ * Basandosi sulla stessa logica delle altre viste, anche qui si fa un'unica vista per questi due contatori,
+ * sfruttando il raggruppamento dei record in base alla tipologia di conto e di transazione 
+ * (in uscita ->  tipo_transazione = 'spesa', in entrata -> tipo_transazione = 'accredito').
+ */
 
 drop view if exists tot_trans_tipologia;
 create view tot_trans_tipologia as (
@@ -349,17 +223,40 @@ select
 	order by id_cliente asc
 );
 
+-- check
 select * from tot_trans_tipologia limit 50;
 
 
 /**
  * 
  * 
- * TABELLA FINALE
+ * Parte 2 - creazione della tabella finale e caricamento dei record nella stessa.
  * 
  * 
  */
 
+
+/*
+ * In questa parte la strategia seguita consiste prima nel creare la tabella vuota e successivamente inserire i valori colonna per colonna 
+ * (ogni colonna rappresenta una feature richiesta).
+ * 
+ * In termini tecnici inizialmente si esegue una insert pescando i dati dalla prima view calcolata, in modo che le colonne dalla terza in poi siano 
+ * inizializzate con i valori di default, e successivamente si eseguono una serie di update, dove in ogni update si aggiorna il valore di una colonna
+ * specifica usando la vista appropriata. 
+ * 
+ * Nota importante:
+ * Ispezionando le viste create nella parte 1, si può vedere come non tutti gli id_cliente siano presenti:
+ * ad esempio nella vista 'num_tot_conti' non è presente id_cliente = 0, in quanto tale cliente non ha conti associati e pertanto viene filtrato
+ * dalle (inner) join eseguite nella definizione della vista. 
+ * 
+ * Poichè è richiesto di fornire un record per ogni cliente registrato (anche senza conti e transazioni associati), si usa questo workaround: 
+ * nella tabella finale si inizializzano i contatori a 0 e poi con i successivi update i contatori si aggiorneranno solo dove c'è un valore diverso da 0.
+ * Il risultato finale sarà che nella tabella sono presenti tutti i clienti, ma quelli non presenti nelle viste manterranno il valore di default 0 
+ * (che corrisponde con il valore atteso in questi casi per la natura dei contatori), mentre tutti gli altri avranno il valore assegnato dall'update.
+ * Questo discorso NON si applica all'età del cliente, che invece prevede sempre un valore inserito.
+ */
+
+-- per il significato delle singole colonne si rimanda agli update sottostanti dove viene sempre indicato con un commento quale indicatore si sta calcolando.
 drop table if exists tab_finale;
 create table tab_finale (
 id_cliente int not null,
@@ -391,27 +288,45 @@ importo_in_privati double default 0.0,
 importo_in_famiglie double default 0.0
 );
 
+-- Età del cliente e id_cliente
+
+/**
+ * Tramite questa insert vengono creati i record per la prima volta e quindi vengono anche inizializzati gli altri contatori al valore di default.
+ */
 insert into tab_finale (id_cliente, eta) 
 	select
 		id_cliente,eta
 	from cliente_tmp;
 
+/**
+ * NOTA TECNICA
+ * Gli update sottostanti dovrebbero essere eseguiti con la logica dell' UPDATE ... SET ... FROM in modo da prendere i valori da 
+ * una suquery eseguita nel from. 
+ * Esempio:
+ *
+ *	update tab_finale tf set tf.num_trans_out_all = t.value
+ *	from 
+ *	(	
+ *		select n.id_cliente, n.value
+ *		from num_trans n
+ *		where n.tipo = 'spesa'
+ *	) as t
+ *	where tf.id_cliente = t.id_cliente; 
+ * 
+ * Tuttavia questa sintassi non è supportata dall'engine MySql qui utilizzato (funziona invece in SQL server).
+ * In MySql è possibile ottenere lo stesso risultato utilizzando un workaround facendo l'update su una join.
+ * Per altri dettagli si veda: https://stackoverflow.com/questions/65877833/is-update-set-from-syntax-supported-in-mysql?noredirect=1&lq=1
+ * 
+ * NOTA SU POSSIBILI WARNING DA PARTE DEL CLIENT
+ * Alcuni client (come DBeaver) possono presentare un warning poichè si esegue un update senza clausola where; si possono eseguire le query
+ * senza problemi in quanto qui il filtro sull'update è applicato implicitamente dalla (inner) join.
+ * 
+ */
 
+-- le join sottostanti sono da intendersi come inner join (default di MySql)
 
--- in MySql non funziona l'update ... from (funziona in sql server)...
--- https://stackoverflow.com/questions/65877833/is-update-set-from-syntax-supported-in-mysql?noredirect=1&lq=1
-/*
-update tab_finale tf set tf.num_trans_out_all = t.value
-from 
-(	
-	select n.id_cliente, n.value
-	from num_trans n
-	where n.tipo = 'spesa'
-) as t
-where tf.id_cliente = t.id_cliente; 
-*/
+--  Numero di transazioni in uscita su tutti i conti
 
--- si deve far un workaround tramite join
 UPDATE tab_finale tf
 JOIN (
     SELECT n.id_cliente, n.value
@@ -420,6 +335,7 @@ JOIN (
 ) AS t ON tf.id_cliente = t.id_cliente
 SET tf.num_trans_out_all = t.value;
 
+-- Numero di transazioni in entrata su tutti i conti
 
 UPDATE tab_finale tf
 JOIN (
@@ -429,7 +345,7 @@ JOIN (
 ) AS t ON tf.id_cliente = t.id_cliente
 SET tf.num_trans_in_all = t.value;
 
--- totale transazioni (importo)
+-- Importo totale transato in uscita su tutti i conti
 
 UPDATE tab_finale tf
 JOIN (
@@ -439,6 +355,8 @@ JOIN (
 ) AS t ON tf.id_cliente = t.id_cliente
 SET tf.importo_out_all = round(t.totale,2);
 
+-- Importo totale transato in entrata su tutti i conti
+
 UPDATE tab_finale tf
 JOIN (
     SELECT tot.id_cliente, tot.totale
@@ -447,8 +365,8 @@ JOIN (
 ) AS t ON tf.id_cliente = t.id_cliente
 SET tf.importo_in_all = round(t.totale,2);
 
+-- Numero totale di conti posseduti
 
--- numero conti
 update tab_finale tf
 join (
 	select id_cliente, num_accounts
@@ -456,7 +374,7 @@ join (
 ) as t on tf.id_cliente = t.id_cliente
 set tf.num_conti_tot = t.num_accounts;
 
--- numero conti per tipologia  num_tot_conti_tipologia
+--  Numero di conti posseduti per tipologia (un indicatore per ogni tipo di conto)
 
 -- base
 update tab_finale tf 
@@ -494,7 +412,7 @@ join (
 ) as t on tf.id_cliente = t.id_cliente
 set tf.num_conti_famiglie = t.num_accounts;
 
--- Numero di transazioni in uscita per tipologia di conto num_trans_tipologia
+-- Numero di transazioni in uscita per tipologia di conto (un indicatore per tipo di conto)
 
 -- base
 update tab_finale tf
@@ -533,7 +451,7 @@ join (
 ) as t on tf.id_cliente = t.id_cliente
 set tf.num_trans_out_famiglie = t.num_transazioni;
 
--- Numero di transazioni in entrata per tipologia di conto [num_trans_tipologia]
+-- Numero di transazioni in entrata per tipologia di conto (un indicatore per tipo di conto)
 
 -- base
 update tab_finale tf
@@ -572,7 +490,7 @@ join (
 ) as t on tf.id_cliente = t.id_cliente
 set tf.num_trans_in_famiglie = t.num_transazioni;
 
--- Importo transato in uscita per tipologia di conto [tot_trans_tipologia]
+-- Importo transato in uscita per tipologia di conto (un indicatore per tipo di conto)
 
 -- base
 update tab_finale tf
@@ -611,7 +529,7 @@ join (
 set tf.importo_out_famiglie = round(t.totale,2);
 
 
--- Importo transato in entrata per tipologia di conto 
+-- Importo transato in entrata per tipologia di conto (un indicatore per tipo di conto)
 
 -- base
 update tab_finale tf
